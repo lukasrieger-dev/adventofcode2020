39ab631090ff2467e8c16a0832aea0d7d66bade4
lukasrieger-dev <rieger.lks@gmail.com>
2020-12-09 14:26:27 +0100

    add day 9

---
+++ day9/day9.py
@@ -0,0 +1,48 @@
+def stream(path):
+    with open(path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            yield(int(line))
+        
+# ---- part 1 ----
+lookahead_size = 25
+lookahead = []
+ready = False
+bad_number = -1
+
+for number in stream('input.txt'):
+    if not ready:
+        lookahead.append(number)
+        if len(lookahead) == lookahead_size:
+            ready = True
+        continue
+    lookahead_set = set(lookahead)
+    valid = False
+    for n in lookahead:
+        diff = (number - n)
+        if (diff in lookahead_set) and not (diff == n):
+            valid = True
+            break       
+    if not valid:
+        print(number)
+        bad_number = number
+        break
+    lookahead.pop(0)
+    lookahead.append(number)
+    
+# ---- part 2 ----
+ranges = []
+stop = False
+
+for number in stream('input.txt'):
+    if stop:
+        break
+    next_range = []
+    ranges.append(next_range)
+    
+    for r in ranges:
+        r.append(number)
+        if sum(r) == bad_number:
+            print(min(r) + max(r))
+            stop = True
+            break
